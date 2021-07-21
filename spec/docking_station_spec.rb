0cbf48d5b8932222d9231b1fdb889db2fefed547
ltaylorbrown <lauren2.brown.lb@googlemail.com>
2021-07-21 13:13:48 +0100

    refactored docking station tests'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,11 +4,11 @@ describe DockingStation do
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
-end
 
-describe DockingStation do 
+
   it 'responds to release_bike and working?' do
-    expect(subject).to respond_to :release_bike and :working?
+    bike = subject.release_bike
+    expect(bike).to be_working
   end 
 end 
 
